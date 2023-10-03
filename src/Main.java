8f2df30e5bd67091559f563694abbf3bfc1fd6d6
Denis Siliukov <abhem21rus@mail.ru>
2023-10-03 08:37:52 +0300

    Переменные, задача1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalDate;
+import org.apache.commons.lang3.StringUtils;
 public class Main {
+    // BEGIN
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0f;
+        var cat = 3.6f;
+        var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
     }
 }
+// END
+
